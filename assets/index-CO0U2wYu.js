a0a925fad21b8c693e2e061774e311b0c8978c2a
SuperYoshi6 <janschillemat@gmail.com>
2026-06-28 21:23:22 +0200

    Updates

--- assets/index-CO0U2wYu.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-cKM1ccli.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-cm7-xogN.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-v5s9_gtS.js";const t=i("PushNotifications",{});export{t as PushNotifications};
