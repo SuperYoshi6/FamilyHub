e9b5186b37f2e22f6279dc851a1a03eb4ab95deb
SuperYoshi6 <janschillemat@gmail.com>
2026-07-05 11:05:12 +0200

    Updates

--- assets/index-B3Pgzs44.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-Dr6YyrsP.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,11 @@
-// Scripts for firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(Promise.resolve());
+  self.skipWaiting();
+});
+
 firebase.initializeApp({
   apiKey: "AIzaSyBbfFwbszHY1GOr6nhEXiAgo4MuHMofvQs",
   authDomain: "familyhub-notification.firebaseapp.com",
@@ -15,12 +18,10 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/logo.png'
+    icon: '/icon.png'
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
